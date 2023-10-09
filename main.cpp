b12c7501951ce5578c6db5dfa357f8f11291e71a
José Rondón <107903082+Rondnt@users.noreply.github.com>
2023-10-08 22:30:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <sstream>
 #include <bitset>
 #include <stdexcept>
+#include <iomanip>
 
 struct Subnet {
     std::string name;
     std::string network;
     std::string broadcast;
+    int size;
+    int x;
 };
 
 std::string applySubnetMask(const std::string& ip, int prefixLength) {
@@ -30,20 +33,30 @@ std::string applySubnetMask(const std::string& ip, int prefixLength) {
     return result;
 }
 
+void clearBuffer() {
+    char c;
+    while (std::cin.get(c) && c != '\n') {
+        // Leer y descartar caracteres del búfer hasta encontrar un salto de línea
+    }
+}
+
 int main() {
     std::string ip;
+    std::string subnetName;
     char addSubnet;
 
     std::vector<Subnet> subnets;
 
-    do {
-        std::cout << "Ingrese una dirección IP (en formato x.x.x.x o x.x.x.x/y): ";
-        std::cin >> ip;
+    std::cout << "Ingrese una dirección IP (en formato x.x.x.x o x.x.x.x/y): ";
+    std::cin >> ip;
 
+    do {
         Subnet subnet;
 
+        clearBuffer(); // Limpiar el búfer de entrada
+
         std::cout << "Ingrese el nombre de la subred: ";
-        std::cin >> subnet.name;
+        std::getline(std::cin, subnetName);
 
         int prefixLength;
         std::cout << "Ingrese la longitud del prefijo (CIDR): ";
@@ -58,11 +71,16 @@ int main() {
                 bits[i] = 1;
             }
             subnet.broadcast = std::to_string(static_cast<unsigned long>(bits.to_ulong()));
+
+            subnet.size = 32 - prefixLength;
+            subnet.x = subnet.size + 2;
+
         } catch (const std::invalid_argument& e) {
             std::cerr << "Error: Dirección IP o máscara inválida. Asegúrate de ingresar una dirección IP válida y una longitud de prefijo válida.\n";
             return 1;
         }
 
+        subnet.name = subnetName;
         subnets.push_back(subnet);
 
         std::cout << "¿Desea agregar otra subred? (y/n): ";
@@ -71,9 +89,10 @@ int main() {
 
     // Mostrar la tabla de subredes
     std::cout << "\nTabla de subredes:\n";
-    std::cout << "Nombre\t\tRed\t\tBroadcast\n";
+    std::cout << std::left << std::setw(20) << "SUBRED" << std::setw(10) << "SIZE" << std::setw(10) << "X" << std::setw(10) << "BLOCK" << std::setw(25) << "MASK" << "\n";
     for (const Subnet& subnet : subnets) {
-        std::cout << subnet.name << "\t\t" << subnet.network << "\t" << subnet.broadcast << "\n";
+        std::cout << std::left << std::setw(20) << subnet.name << std::setw(10) << subnet.size << std::setw(10) << subnet.x << std::setw(10) << " /" << subnet.size << " ("
+                  << applySubnetMask("255.255.255.255", subnet.size) << ")\n";
     }
 
     return 0;
