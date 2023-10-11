39a36b2210918b283bf2932d812a7eeab0e22310
José Rondón <107903082+Rondnt@users.noreply.github.com>
2023-10-11 02:58:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -56,6 +56,26 @@ std::string calculateSubnetMask(int prefixLength) {
     return mask;
 }
 
+std::string calculateNetworkAddress(const std::string& ip, int prefixLength) {
+    std::string maskedIp = applySubnetMask(ip, 32 - prefixLength);
+    return maskedIp;
+}
+
+std::string calculateBroadcastAddress(const std::string& networkAddress, int prefixLength) {
+    std::string broadcastAddress = networkAddress;
+
+    // Calculate the last octet if there are remaining bits.
+    int remainingBits = prefixLength - (32 - networkAddress.size()) + 1;
+    if (remainingBits > 0) {
+        int lastOctetMask = (0xFF >> (8 - remainingBits)) & 0xFF;
+        int lastOctet = std::stoi(networkAddress.substr(networkAddress.size() - 3));
+        int broadcastOctet = lastOctet | lastOctetMask;
+        broadcastAddress = networkAddress.substr(0, networkAddress.size() - 3) + std::to_string(broadcastOctet);
+    }
+
+    return broadcastAddress;
+}
+
 void clearBuffer() {
     char c;
     while (std::cin.get(c) && c != '\n') {
@@ -65,7 +85,6 @@ void clearBuffer() {
 
 int main() {
     std::string ip;
-    std::string subnetName;
     char addSubnet;
 
     std::vector<Subnet> subnets;
@@ -79,10 +98,10 @@ int main() {
         clearBuffer(); // Limpiar el búfer de entrada
 
         std::cout << "Ingrese el nombre de la subred: ";
-        std::getline(std::cin, subnetName);
+        std::getline(std::cin, subnet.name);
 
         int prefixLength;
-        std::cout << "Ingrese la longitud del prefijo (CIDR): ";
+        std::cout << "Ingrese la longitud del prefijo (CIDR) para la subred " << subnet.name << ": ";
         std::cin >> prefixLength;
 
         try {
@@ -102,12 +121,11 @@ int main() {
             // Calcular la máscara como "32 - x"
             int mask = 32 - x;
             subnet.mask = "/" + std::to_string(mask) + " (" + calculateSubnetMask(mask) + ")";
-            subnet.name = subnetName; // Asignar el nombre aquí
 
             subnets.push_back(subnet);
 
         } catch (const std::invalid_argument& e) {
-            std::cerr << "Error: Dirección IP o máscara inválida. Asegúrate de ingresar una dirección IP válida y una longitud de prefijo válida.\n";
+            std::cerr << "Error: Longitud del prefijo inválida. Asegúrate de ingresar una longitud de prefijo válida.\n";
             return 1;
         }
 
@@ -115,7 +133,7 @@ int main() {
         std::cin >> addSubnet;
     } while (addSubnet == 'y' || addSubnet == 'Y');
 
-    // Ordenar la tabla por el tamaño (size) en orden descendente
+    // Ordenar la tabla de subredes por tamaño (size) en orden descendente
     std::sort(subnets.begin(), subnets.end(), [](const Subnet& a, const Subnet& b) {
         return a.size > b.size;
     });
@@ -126,6 +144,34 @@ int main() {
 
     for (const Subnet& subnet : subnets) {
         std::cout << std::left << std::setw(20) << subnet.name << std::setw(10) << subnet.size << std::setw(10) << subnet.x << std::setw(10) << subnet.block << std::setw(20) << subnet.mask << "\n";
+    }
+
+    // Mostrar la tabla de cálculo de Direcciones de Red y Broadcast
+    std::cout << "\nTabla de cálculo de Direcciones de Red y Broadcast:\n";
+    std::cout << std::left << std::setw(20) << "SUBRED" << std::setw(20) << "IP FORMAT" << std::setw(20) << "NETWORK ADD" << std::setw(20) << "BROADCAST ADD" << "\n";
+
+    std::string ipFormat = "172.00010000.00000000."; // Prefix for IP Format
+    std::string networkAdd = "172.16.0.";
+    int currentAddress = 0;
+
+    for (const Subnet& subnet : subnets) {
+        std::string ipFormatLine = ipFormat;
+        std::string networkAddLine = networkAdd + std::to_string(currentAddress);
+
+        for (int i = 0; i < subnet.x; i++) {
+            ipFormatLine += "1";
+        }
+
+        for (int i = subnet.x; i < 6; i++) {
+            ipFormatLine += "0";
+        }
+
+        int broadcast = currentAddress + subnet.block - 1;
+        std::string broadcastAddLine = networkAdd + std::to_string(broadcast);
+
+        std::cout << std::left << std::setw(20) << subnet.name << std::setw(20) << ipFormatLine << std::setw(20) << networkAddLine << std::setw(20) << broadcastAddLine << "\n";
+
+        currentAddress += subnet.block;
     }
 
     return 0;
